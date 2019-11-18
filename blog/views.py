fddb13495dd8b24f71ca56bebaca38c9020cba41
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-11-18 18:24:15 +0200

    Second commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+posts = [{
+    'author': 'Timothy Ngorima',
+    'title' : 'Using Flask WTF',
+    'content' : 'Hey there mirai timbotaura nezve flask',
+    'date_posted' : 'AUgust 24 1995',
+}, 
+{
+    'author': 'Tafara Ngorima',
+    'title' : 'Using Flask WTF',
+    'content' : 'Hey there mirai timbotaura nezve flask',
+    'date_posted' : 'AUgust 24 1995',
+}, 
+{
+    'author': 'Batsirai Ngorima',
+    'title' : 'Using Flask WTF',
+    'content' : 'Hey there mirai timbotaura nezve flask',
+    'date_posted' : 'AUgust 24 1995',
+}]
+
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
