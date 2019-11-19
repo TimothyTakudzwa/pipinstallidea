e968fbde03d4f7de36309b5699fafecd3bae64d3
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-11-19 23:06:46 +0200

    Adding a new users models to the project

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .constants import posts
+# from .constants import posts
+from .models import Post
+
 def home(request):
+   
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
